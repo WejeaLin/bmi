72d0d6461ee188b945f574e6519beca5353925d2
WejeaLin <75632978+WejeaLin@users.noreply.github.com>
2020-12-12 22:55:12 +0800

    initial commit

---
+++ javalib/src/main/java/com/example/javalib/HelloWorld.kt
@@ -0,0 +1,13 @@
+package com.example.javalib
+
+fun main() {
+    print("HelloWorld!")
+    val w =65f
+    val h=1.7f
+    val bmi=w/(h*h)
+    print("you BMI is $bmi")
+}
+
+public class HelloWorld{
+
+}
